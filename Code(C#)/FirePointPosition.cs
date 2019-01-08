af8cb31440d6e7a62fb00b04cd6a9752a4e3fd15
root <root@localhost.localdomain>
2019-01-08 13:37:54 -0800

    First Commit

---
+++ Code(C#)/FirePointPosition.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirePointPosition : MonoBehaviour {
+
+   //private Vector2 tempPos;
+
+	// Use this for initialization
+	void Start () {
+
+        //tempPos = transform.position;
+ 
+		
+	}
+	
+	// Update is called once per frame
+	//void Update () {
+
+        //if (Input.GetKey(KeyCode.D))
+        //{
+            //transform.position.Set(1.4, 0);
+        //}
+    
+        //if (Input.GetKey(KeyCode.A))
+        //{
+            //tempPos.x += -2.8f;
+        //}
+
+    //}
+}
---
+++ Code(C#)/ForeGround.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForeGround : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Renderer>().sortingLayerName = "Everything Else";
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
